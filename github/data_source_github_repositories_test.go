8a98ddc4a8e90a6211d437cb4ae205825a9f0965
Jeremy Udit <jcudit@github.com>
2020-08-04 15:46:54 -0400

    run one test for now

--- github/data_source_github_repositories_test.go
+++ github/data_source_github_repositories_test.go
@@ -52,41 +52,41 @@ func TestAccGithubRepositoriesDataSource(t *testing.T) {
 
 	t.Run("returns an empty list given an invalid query", func(t *testing.T) {
 
-		config := `
-			data "github_repositories" "test" {
-				query = "klsafj_23434_doesnt_exist"
-			}
-		`
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr("data.github_repositories.test", "full_names.#", "0"),
-			resource.TestCheckResourceAttr("data.github_repositories.test", "names.#", "0"),
-		)
-
-		testCase := func(t *testing.T, mode string) {
-			resource.Test(t, resource.TestCase{
-				PreCheck:  func() { skipUnlessMode(t, mode) },
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: config,
-						Check:  check,
-					},
-				},
-			})
-		}
-
-		t.Run("with an anonymous account", func(t *testing.T) {
-			testCase(t, anonymous)
-		})
-
-		t.Run("with an individual account", func(t *testing.T) {
-			testCase(t, individual)
-		})
-
-		t.Run("with an organization account", func(t *testing.T) {
-			testCase(t, organization)
-		})
+		// config := `
+		// 	data "github_repositories" "test" {
+		// 		query = "klsafj_23434_doesnt_exist"
+		// 	}
+		// `
+		//
+		// check := resource.ComposeTestCheckFunc(
+		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "full_names.#", "0"),
+		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "names.#", "0"),
+		// )
+		//
+		// testCase := func(t *testing.T, mode string) {
+		// 	resource.Test(t, resource.TestCase{
+		// 		PreCheck:  func() { skipUnlessMode(t, mode) },
+		// 		Providers: testAccProviders,
+		// 		Steps: []resource.TestStep{
+		// 			{
+		// 				Config: config,
+		// 				Check:  check,
+		// 			},
+		// 		},
+		// 	})
+		// }
+		//
+		// t.Run("with an anonymous account", func(t *testing.T) {
+		// 	testCase(t, anonymous)
+		// })
+		//
+		// t.Run("with an individual account", func(t *testing.T) {
+		// 	testCase(t, individual)
+		// })
+		//
+		// t.Run("with an organization account", func(t *testing.T) {
+		// 	testCase(t, organization)
+		// })
 
 	})
 }
