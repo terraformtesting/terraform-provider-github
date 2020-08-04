4af52dd58762263783985822eec6134183516041
Jeremy Udit <jcudit@github.com>
2020-08-03 21:12:31 -0400

    fixup! align `TestAccGithubRepositories`

--- github/resource_github_repository_test.go
+++ github/resource_github_repository_test.go
@@ -1,90 +1,116 @@
 package github
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccGithubRepositories(t *testing.T) {
 
+	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+
 	t.Run("creates and updates repositories without error", func(t *testing.T) {
 
-		// config := fmt.Sprintf(`
-		// 	data "github_repositories" "test" {
-		// 		query = "org:%s repository:test-repo"
-		// 	}
-		// `, testOrganization)
-		//
-		// check := resource.ComposeTestCheckFunc(
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "full_names.0", regexp.MustCompile(`^`+testOrganization)),
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "names.0", regexp.MustCompile(`^test`)),
-		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "sort", "updated"),
-		// )
-		//
-		// testCase := func(t *testing.T, mode string) {
-		// 	resource.Test(t, resource.TestCase{
-		// 		PreCheck:  func() { skipUnlessMode(t, mode) },
-		// 		Providers: testAccProviders,
-		// 		Steps: []resource.TestStep{
-		// 			{
-		// 				Config: config,
-		// 				Check:  check,
-		// 			},
-		// 		},
-		// 	})
-		// }
-		//
-		// t.Run("with an anonymous account", func(t *testing.T) {
-		// 	testCase(t, anonymous)
-		// })
-		//
-		// t.Run("with an individual account", func(t *testing.T) {
-		// 	testCase(t, individual)
-		// })
-		//
-		// t.Run("with an organization account", func(t *testing.T) {
-		// 	testCase(t, organization)
-		// })
+		config := fmt.Sprintf(`
+			resource "github_repository" "foo" {
+
+			  name         = "tf-acc-test-%[1]s"
+			  description  = "Terraform acceptance tests %[1]s"
+			  homepage_url = "http://example.com/"
+			  visibility = "public"
+				is_template        = false
+
+			  has_issues         = true
+			  has_wiki           = true
+			  has_downloads      = true
+			  allow_merge_commit = true
+			  allow_squash_merge = false
+			  allow_rebase_merge = false
+			  auto_init          = false
+
+			}
+		`, randomID)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("resource.github_repository.foo", "has_issues", "false"),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
 
 	})
 
 	t.Run("imports repositories without error", func(t *testing.T) {
 
-		// config := fmt.Sprintf(`
-		// 	data "github_repositories" "test" {
-		// 		query = "org:%s repository:test-repo"
-		// 	}
-		// `, testOrganization)
-		//
-		// check := resource.ComposeTestCheckFunc(
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "full_names.0", regexp.MustCompile(`^`+testOrganization)),
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "names.0", regexp.MustCompile(`^test`)),
-		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "sort", "updated"),
-		// )
-		//
-		// testCase := func(t *testing.T, mode string) {
-		// 	resource.Test(t, resource.TestCase{
-		// 		PreCheck:  func() { skipUnlessMode(t, mode) },
-		// 		Providers: testAccProviders,
-		// 		Steps: []resource.TestStep{
-		// 			{
-		// 				Config: config,
-		// 				Check:  check,
-		// 			},
-		// 		},
-		// 	})
-		// }
-		//
-		// t.Run("with an anonymous account", func(t *testing.T) {
-		// 	testCase(t, anonymous)
-		// })
-		//
-		// t.Run("with an individual account", func(t *testing.T) {
-		// 	testCase(t, individual)
-		// })
-		//
-		// t.Run("with an organization account", func(t *testing.T) {
-		// 	testCase(t, organization)
-		// })
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+			  name         = "tf-acc-test-%[1]s"
+			  description  = "Terraform acceptance tests %[1]s"
+			  visibility 	 = "public"
+			}
+		`, randomID)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttrSet("resource.github_repository.test", "name"),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+					{
+						ResourceName:      "github_repository.test",
+						ImportState:       true,
+						ImportStateVerify: true,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
 
 	})
 
@@ -334,4 +360,37 @@ func TestAccGithubRepositories(t *testing.T) {
 
 	})
 
+}
+
+func testSweepRepositories(region string) error {
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+
+	repos, _, err := client.Repositories.List(context.TODO(), meta.(*Owner).name, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range repos {
+		if name := r.GetName(); strings.HasPrefix(name, "tf-acc-") || strings.HasPrefix(name, "foo-") {
+			log.Printf("Destroying Repository %s", name)
+
+			if _, err := client.Repositories.Delete(context.TODO(), meta.(*Owner).name, name); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	resource.AddTestSweepers("github_repository", &resource.Sweeper{
+		Name: "github_repository",
+		F:    testSweepRepositories,
+	})
 }
