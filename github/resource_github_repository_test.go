b05b266b9f950945bd2f12f0bb47fee8e1805644
Jeremy Udit <jcudit@github.com>
2020-08-06 06:59:40 -0400

    fixup! correct test case values

--- github/resource_github_repository_test.go
+++ github/resource_github_repository_test.go
@@ -227,10 +227,10 @@ func TestAccGithubRepositories(t *testing.T) {
 
 		checks := map[string]resource.TestCheckFunc{
 			"before": resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr("github_repository.test", "default_branch", "development"),
+				resource.TestCheckResourceAttr("github_repository.test", "default_branch", "master"),
 			),
 			"after": resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr("github_repository.test", "default_branch", "staging"),
+				resource.TestCheckResourceAttr("github_repository.test", "default_branch", "default"),
 			),
 		}
 
@@ -268,43 +268,6 @@ func TestAccGithubRepositories(t *testing.T) {
 	})
 
 	t.Run("manages the license and gitignore feature for a repository", func(t *testing.T) {
-
-		// config := fmt.Sprintf(`
-		// 	data "github_repositories" "test" {
-		// 		query = "org:%s repository:test-repo"
-		// 	}
-		// `, testOrganization)
-		//
-		// check := resource.ComposeTestCheckFunc(
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "full_names.0", regexp.MustCompile(`^`+testOrganization)),
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "names.0", regexp.MustCompile(`^test`)),
-		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "sort", "updated"),
-		// )
-		//
-		// testCase := func(t *testing.T, mode string) {
-		// 	resource.Test(t, resource.TestCase{
-		// 		PreCheck:  func() { skipUnlessMode(t, mode) },
-		// 		Providers: testAccProviders,
-		// 		Steps: []resource.TestStep{
-		// 			{
-		// 				Config: config,
-		// 				Check:  check,
-		// 			},
-		// 		},
-		// 	})
-		// }
-		//
-		// t.Run("with an anonymous account", func(t *testing.T) {
-		// 	testCase(t, anonymous)
-		// })
-		//
-		// t.Run("with an individual account", func(t *testing.T) {
-		// 	testCase(t, individual)
-		// })
-		//
-		// t.Run("with an organization account", func(t *testing.T) {
-		// 	testCase(t, organization)
-		// })
 
 	})
 
