43b6cb9149df59186cd554161008975603f54afc
Jeremy Udit <jcudit@github.com>
2020-08-06 07:37:55 -0400

    creates a repository using a template

--- github/resource_github_repository_test.go
+++ github/resource_github_repository_test.go
@@ -355,42 +355,47 @@ func TestAccGithubRepositories(t *testing.T) {
 
 	t.Run("creates a repository using a template", func(t *testing.T) {
 
-		// config := fmt.Sprintf(`
-		// 	data "github_repositories" "test" {
-		// 		query = "org:%s repository:test-repo"
-		// 	}
-		// `, testOrganization)
-		//
-		// check := resource.ComposeTestCheckFunc(
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "full_names.0", regexp.MustCompile(`^`+testOrganization)),
-		// 	resource.TestMatchResourceAttr("data.github_repositories.test", "names.0", regexp.MustCompile(`^test`)),
-		// 	resource.TestCheckResourceAttr("data.github_repositories.test", "sort", "updated"),
-		// )
-		//
-		// testCase := func(t *testing.T, mode string) {
-		// 	resource.Test(t, resource.TestCase{
-		// 		PreCheck:  func() { skipUnlessMode(t, mode) },
-		// 		Providers: testAccProviders,
-		// 		Steps: []resource.TestStep{
-		// 			{
-		// 				Config: config,
-		// 				Check:  check,
-		// 			},
-		// 		},
-		// 	})
-		// }
-		//
-		// t.Run("with an anonymous account", func(t *testing.T) {
-		// 	testCase(t, anonymous)
-		// })
-		//
-		// t.Run("with an individual account", func(t *testing.T) {
-		// 	testCase(t, individual)
-		// })
-		//
-		// t.Run("with an organization account", func(t *testing.T) {
-		// 	testCase(t, organization)
-		// })
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+				name        = "tf-acc-test-%s"
+				description = "Terraform acceptance tests %[1]s"
+
+				template {
+					owner = "%s"
+					repository = "%s"
+				}
+
+			}
+		`, randomID, testOrganization, "terraform-template-module")
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr("github_repository.test", "is_template", "false"),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
 
 	})
 
