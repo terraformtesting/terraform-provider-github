023c664ddc2eda5159bf690b126120de5fedb68d
Jeremy Udit <jcudit@github.com>
2020-08-14 19:36:04 -0400

    fixup! uncomment passing tests

--- github/data_source_github_release_test.go
+++ github/data_source_github_release_test.go
@@ -3,6 +3,7 @@ package github
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -110,113 +111,113 @@ func TestAccGithubReleaseDataSource(t *testing.T) {
 
 	})
 
-	// t.Run("errors when querying with non-existent ID", func(t *testing.T) {
-	//
-	// 	config := `
-	// 		data "github_release" "test" {
-	// 			repository = "test"
-	// 			owner = "test"
-	// 			retrieve_by = "id"
-	// 		}
-	// 	`
-	//
-	// 	testCase := func(t *testing.T, mode string) {
-	// 		resource.Test(t, resource.TestCase{
-	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
-	// 			Providers: testAccProviders,
-	// 			Steps: []resource.TestStep{
-	// 				{
-	// 					Config:      config,
-	// 					ExpectError: regexp.MustCompile("`release_id` must be set when `retrieve_by` = `id`"),
-	// 				},
-	// 			},
-	// 		})
-	// 	}
-	//
-	// 	t.Run("with an anonymous account", func(t *testing.T) {
-	// 		testCase(t, anonymous)
-	// 	})
-	//
-	// 	t.Run("with an individual account", func(t *testing.T) {
-	// 		testCase(t, individual)
-	// 	})
-	//
-	// 	t.Run("with an organization account", func(t *testing.T) {
-	// 		testCase(t, organization)
-	// 	})
-	//
-	// })
-	//
-	// t.Run("errors when querying with non-existent repository", func(t *testing.T) {
-	//
-	// 	config := `
-	// 		data "github_release" "test" {
-	// 			repository = "test"
-	// 			owner = "test"
-	// 			retrieve_by = "latest"
-	// 		}
-	// 	`
-	// 	testCase := func(t *testing.T, mode string) {
-	// 		resource.Test(t, resource.TestCase{
-	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
-	// 			Providers: testAccProviders,
-	// 			Steps: []resource.TestStep{
-	// 				{
-	// 					Config:      config,
-	// 					ExpectError: regexp.MustCompile(`Not Found`),
-	// 				},
-	// 			},
-	// 		})
-	// 	}
-	//
-	// 	t.Run("with an anonymous account", func(t *testing.T) {
-	// 		testCase(t, anonymous)
-	// 	})
-	//
-	// 	t.Run("with an individual account", func(t *testing.T) {
-	// 		testCase(t, individual)
-	// 	})
-	//
-	// 	t.Run("with an organization account", func(t *testing.T) {
-	// 		testCase(t, organization)
-	// 	})
-	//
-	// })
-	//
-	// t.Run("errors when querying with non-existent tag", func(t *testing.T) {
-	//
-	// 	config := `
-	// 		data "github_release" "test" {
-	// 			repository = "test"
-	// 			owner = "test"
-	// 			retrieve_by = "tag"
-	// 		}
-	// 	`
-	// 	testCase := func(t *testing.T, mode string) {
-	// 		resource.Test(t, resource.TestCase{
-	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
-	// 			Providers: testAccProviders,
-	// 			Steps: []resource.TestStep{
-	// 				{
-	// 					Config:      config,
-	// 					ExpectError: regexp.MustCompile("`release_tag` must be set when `retrieve_by` = `tag`"),
-	// 				},
-	// 			},
-	// 		})
-	// 	}
-	//
-	// 	t.Run("with an anonymous account", func(t *testing.T) {
-	// 		testCase(t, anonymous)
-	// 	})
-	//
-	// 	t.Run("with an individual account", func(t *testing.T) {
-	// 		testCase(t, individual)
-	// 	})
-	//
-	// 	t.Run("with an organization account", func(t *testing.T) {
-	// 		testCase(t, organization)
-	// 	})
-	//
-	// })
+	t.Run("errors when querying with non-existent ID", func(t *testing.T) {
+
+		config := `
+			data "github_release" "test" {
+				repository = "test"
+				owner = "test"
+				retrieve_by = "id"
+			}
+		`
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config:      config,
+						ExpectError: regexp.MustCompile("`release_id` must be set when `retrieve_by` = `id`"),
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			testCase(t, anonymous)
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+
+	t.Run("errors when querying with non-existent repository", func(t *testing.T) {
+
+		config := `
+			data "github_release" "test" {
+				repository = "test"
+				owner = "test"
+				retrieve_by = "latest"
+			}
+		`
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config:      config,
+						ExpectError: regexp.MustCompile(`Not Found`),
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			testCase(t, anonymous)
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+
+	t.Run("errors when querying with non-existent tag", func(t *testing.T) {
+
+		config := `
+			data "github_release" "test" {
+				repository = "test"
+				owner = "test"
+				retrieve_by = "tag"
+			}
+		`
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config:      config,
+						ExpectError: regexp.MustCompile("`release_tag` must be set when `retrieve_by` = `tag`"),
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			testCase(t, anonymous)
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
 
 }
