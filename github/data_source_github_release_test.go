e9f59e867b4b0ca456b45b4a9d05c5d0ed3d0fb7
Jeremy Udit <jcudit@github.com>
2020-08-14 15:32:49 -0400

    fixup! use env vars

--- github/data_source_github_release_test.go
+++ github/data_source_github_release_test.go
@@ -1,6 +1,8 @@
 package github
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -8,19 +10,22 @@ import (
 
 func TestAccGithubReleaseDataSource(t *testing.T) {
 
+	testReleaseRepository := os.Getenv("GITHUB_TEMPLATE_REPOSITORY")
+	testReleaseID := os.Getenv("GITHUB_TEMPLATE_REPOSITORY_RELEASE_ID")
+
 	t.Run("queries latest release", func(t *testing.T) {
 
-		config := `
+		config := fmt.Sprintf(`
 			data "github_release" "test" {
-				repository = "terraform-template-module"
-				owner = "terraformtesting"
+				repository = "%s"
+				owner = "%s"
 				retrieve_by = "latest"
 			}
-		`
+		`, testReleaseRepository, testOwner)
 
 		check := resource.ComposeTestCheckFunc(
 			resource.TestCheckResourceAttr(
-				"data.github_release.test", "id", "25117183",
+				"data.github_release.test", "id", testReleaseID,
 			),
 		)
 
@@ -51,53 +56,50 @@ func TestAccGithubReleaseDataSource(t *testing.T) {
 
 	})
 
-	// t.Run("queries release by ID", func(t *testing.T) {
-	//
-	// 	config := `
-	// 		data "github_release" "test" {
-	// 			repository = "torvalds/linux"
-	// 			owner = "owner"
-	// 			retrieve_by = "id"
-	// 			release_id = "0"
-	// 		}
-	// 	`
-	//
-	// 	check := resource.ComposeTestCheckFunc(
-	// 	// resource.TestCheckResourceAttr(
-	// 	// 	"github_repository_webhook.test", "active", "true",
-	// 	// ),
-	// 	// resource.TestCheckResourceAttr(
-	// 	// 	"github_repository_webhook.test", "events.#", "1",
-	// 	// ),
-	// 	)
-	//
-	// 	testCase := func(t *testing.T, mode string) {
-	// 		resource.Test(t, resource.TestCase{
-	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
-	// 			Providers: testAccProviders,
-	// 			Steps: []resource.TestStep{
-	// 				{
-	// 					Config: config,
-	// 					Check:  check,
-	// 				},
-	// 			},
-	// 		})
-	// 	}
-	//
-	// 	t.Run("with an anonymous account", func(t *testing.T) {
-	// 		testCase(t, anonymous)
-	// 	})
-	//
-	// 	t.Run("with an individual account", func(t *testing.T) {
-	// 		testCase(t, individual)
-	// 	})
-	//
-	// 	t.Run("with an organization account", func(t *testing.T) {
-	// 		testCase(t, organization)
-	// 	})
-	//
-	// })
-	//
+	t.Run("queries release by ID", func(t *testing.T) {
+
+		config := fmt.Sprintf(`
+			data "github_release" "test" {
+				repository = "%s"
+				owner = "%s"
+				retrieve_by = "id"
+				release_id = "%s"
+			}
+		`, testReleaseRepository, testOwner, testReleaseID)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr(
+				"data.github_release.test", "id", testReleaseID,
+			),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			testCase(t, anonymous)
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+
 	// t.Run("queries release by tag", func(t *testing.T) {
 	//
 	// 	config := `
