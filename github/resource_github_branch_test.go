6e2a15ce45ae4dccba51d3635ef46739b9dc2846
Jeremy Udit <jcudit@github.com>
2020-08-15 07:37:00 -0400

    fixup! remove delete test

--- github/resource_github_branch_test.go
+++ github/resource_github_branch_test.go
@@ -113,46 +113,4 @@ func TestAccGithubBranch(t *testing.T) {
 
 	})
 
-	// t.Run("deletes a branch without error", func(t *testing.T) {
-	//
-	// 	config := fmt.Sprintf(`
-	// 		resource "github_repository" "test" {
-	// 		  name = "tf-acc-test-%s"
-	// auto_init = true
-	// 		}
-	//
-	// 		resource "github_branch" "test" {
-	// 			repository = github_repository.test.id
-	// 			branch     = "tf-acc-test-%[1]s"
-	// 		}
-	// 	`, randomID)
-	//
-	// 	testCase := func(t *testing.T, mode string) {
-	// 		resource.Test(t, resource.TestCase{
-	// 			PreCheck:  func() { skipUnlessMode(t, mode) },
-	// 			Providers: testAccProviders,
-	// 			Steps: []resource.TestStep{
-	// 				{
-	// 					Config:             config,
-	// 					Destroy:            true,
-	// 					ExpectNonEmptyPlan: true,
-	// 				},
-	// 			},
-	// 		})
-	// 	}
-	//
-	// 	t.Run("with an anonymous account", func(t *testing.T) {
-	// 		t.Skip("anonymous account not supported for this operation")
-	// 	})
-	//
-	// 	t.Run("with an individual account", func(t *testing.T) {
-	// 		testCase(t, individual)
-	// 	})
-	//
-	// 	t.Run("with an organization account", func(t *testing.T) {
-	// 		testCase(t, organization)
-	// 	})
-	//
-	// })
-
 }
