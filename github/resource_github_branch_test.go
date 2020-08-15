d356b0d954099fb85ec7e130a52593ca07925fc8
Jeremy Udit <jcudit@github.com>
2020-08-15 07:20:19 -0400

    fixup! break into two tests

--- github/resource_github_branch_test.go
+++ github/resource_github_branch_test.go
@@ -12,7 +12,7 @@ func TestAccGithubBranch(t *testing.T) {
 
 	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
 
-	t.Run("creates a branch directly or from a source", func(t *testing.T) {
+	t.Run("creates a branch directly", func(t *testing.T) {
 
 		config := fmt.Sprintf(`
 			resource "github_repository" "test" {
@@ -24,20 +24,64 @@ func TestAccGithubBranch(t *testing.T) {
 			  repository = github_repository.test.id
 			  branch     = "tf-acc-test-%[1]s"
 			}
-
-			resource "github_branch" "test_from_source_branch" {
-			  repository = github_repository.test.id
-			  source_branch = "tf-acc-test-%[1]s"
-			  branch        = "tf-acc-test-%[1]s-from-source"
-			}
 		`, randomID)
 
 		check := resource.ComposeTestCheckFunc(
 			resource.TestCheckResourceAttr(
 				"github_branch.test", "id", randomID,
 			),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+
+	t.Run("creates a branch from a source branch", func(t *testing.T) {
+
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+			  name = "tf-acc-test-%[1]s"
+				auto_init = true
+			}
+
+			resource "github_branch" "source" {
+			  repository = github_repository.test.id
+			  branch     = "tf-acc-test-%[1]s-source"
+			}
+
+			resource "github_branch" "test" {
+			  repository 		= github_repository.test.id
+			  source_branch = github_branch.source.id
+			  branch        = "tf-acc-test-%[1]s"
+			}
+		`, randomID)
+
+		check := resource.ComposeTestCheckFunc(
 			resource.TestCheckResourceAttr(
-				"github_branch.test_from_source_branch", "id", randomID,
+				"github_branch.test", "id", randomID,
 			),
 		)
 
