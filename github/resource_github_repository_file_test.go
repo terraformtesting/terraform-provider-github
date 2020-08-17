d55c36bbd02bbd19e744483b9bf310b0ccf5ca83
Jeremy Udit <jcudit@github.com>
2020-08-17 19:35:24 -0400

    fixup! manages file content for a specified branch

--- github/resource_github_repository_file_test.go
+++ github/resource_github_repository_file_test.go
@@ -14,7 +14,7 @@ func TestAccGithubRepositoryFile(t *testing.T) {
 
 	randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
 
-	t.Run("creates and updates secrets without error", func(t *testing.T) {
+	t.Run("creates and updates file content without error", func(t *testing.T) {
 
 		fileContent := "file_content_value"
 		updatedFileContent := "updated_file_content_value"
@@ -61,6 +61,61 @@ func TestAccGithubRepositoryFile(t *testing.T) {
 							fileContent,
 							updatedFileContent, 1),
 						Check: checks["after"],
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+
+	})
+
+	t.Run("manages file content for a specified branch", func(t *testing.T) {
+
+		config := fmt.Sprintf(`
+			resource "github_repository" "test" {
+			  name = "tf-acc-test-%s"
+				auto_init = true
+			}
+
+			resource "github_branch" "test" {
+			  repository = github_repository.test.id
+			  branch     = "tf-acc-test-%[1]s"
+			}
+
+			resource "github_repository_file" "test" {
+			  repository = github_repository.test.id
+			  branch     = github_branch.test.branch
+			  file       = "test"
+			  content    = "test"
+			}
+		`, randomID)
+
+		check := resource.ComposeTestCheckFunc(
+			resource.TestCheckResourceAttr(
+				"github_repository_file.test", "sha",
+				"deee258b7c807901aad79d01da020d993739160a",
+			),
+		)
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: config,
+						Check:  check,
 					},
 				},
 			})
